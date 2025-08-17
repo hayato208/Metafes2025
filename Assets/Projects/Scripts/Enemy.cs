7fa1c99bb9412167a424064c11dc685c550e9b4b
hayato_win11 <hayato2081002@gmail.com>
2025-08-17 16:37:59 +0900

    エネミー消失を実装

---
+++ Assets/Projects/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Enemy : UdonSharpBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        // 衝突相手に Bullet コンポーネントが付いているかチェック
+        Bullet bullet = other.gameObject.GetComponent<Bullet>();
+        if (bullet != null)
+        {
+            // 弾と自分を削除
+            Destroy(gameObject);  // 自分(敵)を消す
+            Destroy(other.gameObject);  // 弾を消す
+        }
+    }
+}
