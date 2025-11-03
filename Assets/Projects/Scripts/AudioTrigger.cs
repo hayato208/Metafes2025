ceed27484a65d1fd87661c369acb86cf835756f6
hayato_win11 <hayato2081002@gmail.com>
2025-11-03 23:59:10 +0900

    ゲーム開始前の町内放送

---
+++ Assets/Projects/Scripts/AudioTrigger.cs
@@ -0,0 +1,31 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class AudioTrigger : UdonSharpBehaviour
+{
+    public AudioSource audioSource; // 再生するAudioSource
+    public bool isInAreaBGM = false;   // エリア内のみBGM流すか
+                                       // private bool hasPlayed = false;
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (!player.isLocal) return; // ローカルプレイヤーのみ反応
+
+        // すでに再生中なら何もしない
+        if (audioSource.isPlaying) return;
+
+        audioSource.Play();
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        if (!player.isLocal) return;
+
+        if (isInAreaBGM)
+        {
+            audioSource.Stop();
+        }
+    }
+}
