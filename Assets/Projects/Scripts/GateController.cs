d69fa27449a38f40f2468d39bef8489563d65ce6
hayato_win11 <hayato2081002@gmail.com>
2025-11-07 22:09:19 +0900

    skybox設定 BGM、扉開くときのSE設定

--- Assets/Projects/Scripts/GateController.cs
+++ Assets/Projects/Scripts/GateController.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 public class GateController : UdonSharpBehaviour
 {
     [SerializeField] private GameObject gateObject; // 開閉対象のオブジェクト
+    [SerializeField] private SoundEffectPlayer soundEffectPlayer; // サウンド
+
+    public bool oneTimeOnly = false; // Inspectorで設定可能
+    private bool hasOpened = false; // このオブジェクトで一度でも開いたかどうか
 
     public void Open()
     {
         if (gateObject != null)
         {
             gateObject.SetActive(false); // 例: 非表示で「通れる」
+
+            // ワールド通じて1回のみ&未開放
+            // 銃をピックアップした1回目のみどうさせる
+            if (oneTimeOnly && hasOpened) return;
+
+            // 扉を開ける音
+            soundEffectPlayer.SendCustomEvent("PlaySE");
+            hasOpened = true; // 開いた状態を記録
         }
     }
 
---
+++ Assets/Projects/Scripts/SoundEffectPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SoundEffectPlayer : UdonSharpBehaviour
+{
+    [Header("再生する効果音（AudioSourceを指定）")]
+    public AudioSource audioSource;
+    // private bool hasPlayed = false;
+
+    // 外部から呼び出す用のメソッド
+    public void PlaySE()
+    {
+        if (audioSource == null) return;
+
+        audioSource.Play();
+      //  hasPlayed = true;
+    }
+}
